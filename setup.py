68e3d74d61fea1369fa0d9eb130ad35cf39473b6
Alexander Collins <alexcollins010@gmail.com>
2025-05-29 16:32:10 +1000

    chore: Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="turbodrf",
-    version="0.1.3",
+    version="0.1.4",
     author="Your Name",
     author_email="your.email@example.com",
     description="Dead simple Django REST API generator with role-based permissions",
