42b5954b6bb8ed651408ec17795877dac6b6fedf
GitHub Action <action@github.com>
2025-05-31 02:59:19 +0000

    chore: release version 0.2.0 [skip ci]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="turbodrf",
-    version="0.1.10",
+    version="0.2.0",
     author="Alexander Collins",
     author_email="",
     description="Dead simple Django REST API generator with role-based permissions",
